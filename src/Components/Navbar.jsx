82592b2b323d39a6e353960043f40857f760151a
Anmol sharma <as7067647@gmail.com>
2025-02-12 12:42:42 +0530

    add one more navlink

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -92,6 +92,12 @@ const Navbar = () => {
       {isOpen && (
         <div className="md:hidden">
           <div className="px-2 pt-2 pb-3 space-y-1">
+          <Link
+              to="/Add Score"
+              className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-[18px] font-mono font-semibold"
+            >
+              AddScore
+            </Link>
             <Link
               to="/Score page"
               className="text-gray-300 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
